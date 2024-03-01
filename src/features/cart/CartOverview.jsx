c3525124afa1737ece656065a5dfa3c5a85f8d95
Florian Miller <flokomi@web.de>
2024-03-01 13:39:16 +0100

    build app with basic api functions and styled with tailwind

---
+++ src/features/cart/CartOverview.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+
+function CartOverview() {
+  return (
+    <div className="flex items-center justify-between bg-stone-800 p-4 uppercase text-stone-200 sm:px-6">
+      <p className="text-sone-300font-semibold space-x-4 text-sm sm:space-x-6 md:text-base">
+        <span>23 pizzas</span>
+        <span>$23.45</span>
+      </p>
+      <Link to="/cart">Open cart &rarr;</Link>
+    </div>
+  );
+}
+
+export default CartOverview;
---
+++ src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+function Username() {
+  return <div className="hidden text-sm font-semibold md:block">Flori</div>;
+}
+
+export default Username;
